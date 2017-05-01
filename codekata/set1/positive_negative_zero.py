39fe7c59fa9bcc8a26bd6c32cbaca1c11687d220
satheshrgs <satheshrgs@gmail.com>
2017-05-01 22:16:32 +0530

    set1-1

---
+++ codekata/set1/positive_negative_zero.py
@@ -0,0 +1,16 @@
+'''
+Created on 01-May-2017
+
+@author: Sathesh Rgs
+'''
+print("Program to check whether a number is positive or negative or zero")
+try:
+    num=int(input("Enter a number.."))
+    if num > 0:
+        print("Positive Number")
+    elif num < 0:
+        print("Negative Number")
+    else:
+        print("The Number is zero")
+except:
+    print("Enter a valid Number")
